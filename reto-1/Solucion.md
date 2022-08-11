ee36b04e70fab56965a1b07843b244c7cdfa3d7a
GeorgeCodde <jorgels120878@gmail.com>
2022-08-11 11:15:59 -0500

    add/reto1

---
+++ reto-1/Solucion.md
@@ -0,0 +1,9 @@
+# Solucion a reto 1
+
+Pantallazo donde se vea la creación del contenedor y podamos comprobar que el contenedor está funcionando.
+
+Pantallazo donde se vea el acceso al servidor web utilizando un navegador web (recuerda que tienes que acceder a la ip del ordenador donde tengas instalado docker)
+
+Pantallazo donde se vean las imágenes que tienes en tu registro local.
+
+Pantallazo donde se vea cómo se elimina el contenedor (recuerda que antes debe estar parado el contenedor).
---
+++ reto-1/scriptReto1.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# contenedor a partir de la imagen nginx , el contenedor se llama 
+# servidor_web y se accede a él utilizando el puerto 8181 
+
+echo -e "\033[0;34mEl siguiente script crea un contenedor nginx"
+echo "Con el nombre servidor_web en el puerto 8181"
+echo -e "\033[0;36m¿Quieres ejecutar el proceso? (y/n)\033[0;37m"
+read runProcess
+if [[ $runProcess == "y" || $runProcess == "Y" ]]
+then
+    docker run -d --name servidor_web -p 8181:80 nginx
+    echo -e "\033[0;33m Contenedores en ejecución \033[0;37m"
+    docker ps
+    echo -e "\033[0;33mImagenes en el host\033[0;37m"
+    docker images
+    echo -e "\033[0;32m--Servidor_web en ejecución http://localhost:8181 --\033[0;37m"
+    echo -e "\033[0;36m¿Quieres detener el servidor_web y eliminar el contenedor? (y/n)\033[0;37m"
+    read stopContainer
+    if [[ $stopContainer == "y" || $stopContainer == "Y" ]]
+    then
+        echo -e "\033[0;33mLista de contenedores en ejecución\033[0;37m"
+        docker ps
+        docker stop servidor_web
+        echo -e "\033[0;31m--Servidor_web detenido--\033[0;37m"
+        echo -e "\033[0;33mLista de contenedores en ejecución\033[0;37m"
+        docker ps
+        docker rm servidor_web
+        echo -e "\033[0;31m--Contenedor Servidor_web eliminado--\033[0;37m"
+        echo -e "\033[0;33mLista de contenedores\033[0;37m"
+        docker ps -a
+    fi
+fi
+
+
+
+
+#
